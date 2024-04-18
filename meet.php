45f10388e10ef420d4f58c76b629afcc3fc13556
Ronny Wang <ronnywang@gmail.com>
2024-04-18 16:16:32 +0800

    增加 ivod 記錄

--- meet.php
+++ meet.php
@@ -18,7 +18,7 @@ $data = json_decode(file_get_contents($url));
 <h1><?= htmlspecialchars($data->name) ?></h1>
 <code>API: <?= htmlspecialchars($url) ?></code>
 <hr>
-跳到：<a href="#section-meet-data">會議資料</a>、<a href="#section-speech">發言紀錄</a>、<a href="#section-gazette">公報發言紀錄</a>
+跳到：<a href="#section-meet-data">會議資料</a>、<a href="#section-speech">發言紀錄</a>、<a href="#section-gazette">公報發言紀錄</a>、<a href="#section-ivod">iVOD記錄</a>
 <hr>
 <h2 id="section-meet-data">會議資料</h2>
 <h4>本區資料來自 <a href="https://data.ly.gov.tw/getds.action?id=42">立法院資料開放平台</a></h4>
@@ -94,6 +94,41 @@ $data = json_decode(file_get_contents($url));
     </tr>
     <?php } ?>
 </table>
+<hr>
+<h2 id="section-ivod">iVOD記錄</h2>
+<h4>以下資料是從<a href="https://ivod.ly.gov.tw">立法院iVOD</a>抓取</h4>
+<?php
+$url = "https://{$domain}/meet/" . urlencode($id) . "/ivod";
+$data = json_decode(file_get_contents($url));
+?>
+<code>API: <?= htmlspecialchars($url) ?></code>
+<table border="1">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>日期</th>
+            <th>委員名稱</th>
+            <th>委員發言時間</th>
+            <th>影片長度</th>
+            <th>連結</th>
+        </tr>
+    </thead>
+    <tbody>
+    <?php foreach ($data->ivods as $ivod) { ?>
+    <tr>
+        <td><?= htmlspecialchars($ivod->id) ?></td>
+        <td><?= htmlspecialchars($ivod->date) ?></td>
+        <td><?= htmlspecialchars($ivod->{'委員名稱'}) ?></td>
+        <td><?= htmlspecialchars($ivod->{'委員發言時間'}) ?></td>
+        <td><?= htmlspecialchars($ivod->{'影片長度'}) ?></td>
+        <td><a href="<?= htmlspecialchars($ivod->url) ?>">Link</a></td>
+
+
+    </tr>
+    <?php } ?>
+    </tbody>
+</table>
+
 
 </body>
 </html>
